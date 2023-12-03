74298b06cdb5ab5043accb5bbc05268c67d5f799
Hare Krishna <shivanshu singh>
2023-12-03 12:54:15 +0530

    first commit

---
+++ app/components/AddTask.tsx
@@ -0,0 +1,9 @@
+
+import {AiOutlinePlus} from 'react-icons/ai'
+const AddTask = () => {
+  return (
+    <button className="btn btn-primary w-full">Add new Task <AiOutlinePlus/></button>
+  )
+}
+
+export default AddTask
